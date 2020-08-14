5a0b79b38549a04b2d5859e014728fd004bd45a2
Emmanuel Espinoza <jose.espinoza@globant.com>
2020-08-14 12:07:12 -0500

    Initial solution and the beginning of HomeController

---
+++ ninja-api/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ninja_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        private static readonly User[] Users = new[]
+        {
+            new User { Id = 1, Name = "Emmanuel Esp", Email = "emmanesp@globant.com" }
+        };
+
+        // GET: api/<HomeController>
+        [HttpGet]
+        public IEnumerable<User> Get()
+        {
+            return Users;
+        }
+
+        // GET api/<HomeController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<HomeController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<HomeController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+    }
+}
