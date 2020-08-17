a6fcb1e74548629cd4b7959350738f11736eca6b
Emmanuel Espinoza <jose.espinoza@globant.com>
2020-08-17 18:27:48 -0500

    UserServices created, dependency injection implemented and HomeController refactored

---
+++ ninja-api/Repository/UserService.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ninja_api.Repository
+{
+    public class UserService : IUserServiceRepository
+    {
+        private static readonly List<User> _users = new List<User>();
+
+        public User CreateUser(User user)
+        {
+            _users.Add(user);
+
+            return user;
+        }
+
+        public User GetUserById(int id)
+        {
+            return _users.SingleOrDefault(user => user.Id == id);
+        }
+
+        public IEnumerable<User> GetUsers()
+        {
+            return _users;
+        }
+
+        public User UpdateUser(int id, User updatedUser)
+        {
+            var user = _users.FirstOrDefault(m => m.Id == id);
+
+            if (user != null)
+            {
+                user.Name = updatedUser.Name;
+                user.Email = updatedUser.Email;
+            }
+
+            return user;
+        }
+    }
+}
--- ninja-api/Startup.cs
+++ ninja-api/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using ninja_api.Repository;
 
 namespace ninja_api
 {
@@ -26,6 +27,7 @@ namespace ninja_api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddScoped<IUserServiceRepository, UserService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
