67307c366f86b462694f7e15159086c991c7f1e2
Francisco Miguel Estrada <francisco.estrada@globant.com>
2020-08-21 09:27:31 -0500

    Swagger Configuration - Modified Put Method on API
    Added swagger configuration and adaptd Put Method with MediatR Pattern in API

--- Ninja.Api/Controllers/UserController.cs
+++ Ninja.Api/Controllers/UserController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Ninja.Application.Common;
 using Ninja.Application.Common.Models;
 using Ninja.Application.Services;
+using Ninja.Application.Users.Commands;
 using Ninja.Application.Users.Queries;
 
 namespace Ninja.Api.Controllers
@@ -28,6 +29,10 @@ namespace Ninja.Api.Controllers
             _mediator = mediator;
         }
 
+        /// <summary>
+        /// Method that retrieves the entire list of users
+        /// </summary>
+        /// <returns></returns>
         [HttpGet]
         public async Task<ActionResult<Response<List<UserVm>>>> Get()
         {
@@ -35,6 +40,11 @@ namespace Ninja.Api.Controllers
             return StatusCode(result.StatusCode, result);
         }
 
+        /// <summary>
+        /// Method that retrieves only 1 user by Id
+        /// </summary>
+        /// <param name="id">User Identifier</param>
+        /// <returns></returns>
         [HttpGet("{id}")]
         public async Task<ActionResult<Response<UserVm>>> Get(int id)
         {
@@ -48,10 +58,17 @@ namespace Ninja.Api.Controllers
         //    return Ok(_userServiceRespository.CreateUser(user));
         //}
 
-        //[HttpPut("{id}")]
-        //public ActionResult<UserVm> Put(int id, [FromBody] UserVm user)
-        //{
-        //    return Ok(_userServiceRespository.UpdateUser(id, user));
-        //}
+        /// <summary>
+        /// Method for update an user record 
+        /// </summary>
+        /// <param name="id">User Identifier</param>
+        /// <param name="user">New user info like Name or Email</param>
+        /// <returns></returns>
+        [HttpPut("{id}")]
+        public async Task<ActionResult<UserVm>> Put(int id, [FromBody] UserVm user)
+        {
+            var result = await _mediator.Send(new UpdateUserByIdCommand(id,user));
+            return StatusCode(result.StatusCode, result);
+        }
     }
 }
--- Ninja.Api/Startup.cs
+++ Ninja.Api/Startup.cs
@@ -4,20 +4,24 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Hosting.Internal;
 using Ninja.Application.Common.Interfaces;
 using Ninja.Application.Middlewares;
 using Ninja.Application.Services;
 using Ninja.Application.Users.Queries;
+using System.IO;
 
 namespace Ninja.Api
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, IWebHostEnvironment _hostingEnvironment)
         {
             Configuration = configuration;
+            hostingEnvironment = _hostingEnvironment;
         }
 
+        private readonly IWebHostEnvironment hostingEnvironment;
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
@@ -28,6 +32,23 @@ namespace Ninja.Api
             services.AddSingleton<ILoggin, Loggin>();
 
             services.AddMediatR(typeof(GetAllUsersQuery));
+
+            services.AddSwaggerGen(
+                config =>
+                config.SwaggerDoc(
+                    "NinjaAPI",
+                    new Microsoft.OpenApi.Models.OpenApiInfo()
+                    {
+                        Title = "Controllers Info"
+                    })
+                );
+
+            string basePath = hostingEnvironment.ContentRootPath;
+
+            services.AddSwaggerGen(
+                config =>
+                config.IncludeXmlComments(Path.Combine(basePath, "Ninja.Api.xml"))
+                );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -49,6 +70,13 @@ namespace Ninja.Api
             {
                 endpoints.MapControllers();
             });
+
+            //Swagger Config
+            app.UseSwagger();
+            app.UseSwaggerUI(
+                config =>
+                config.SwaggerEndpoint("/swagger/NinjaAPI/swagger.json", "API de control de Usuarios")
+                );
         }
     }
 }
