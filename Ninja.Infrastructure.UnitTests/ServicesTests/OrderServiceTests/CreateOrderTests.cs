6a9275e2bcb2ddfe441ab8aea894ddf317b858f3
Josue <josueca18@gmail.com>
2020-09-09 12:36:48 -0500

    services products and orders unitesting

---
+++ Ninja.Infrastructure.UnitTests/ServicesTests/OrderServiceTests/CreateOrderTests.cs
@@ -0,0 +1,31 @@
+﻿using Moq;
+using Ninja.Application.Common.Interfaces;
+using Ninja.Domain.Entities.OrderModel;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ninja.Infrastructure.UnitTests.ServicesTests.OrderServiceTests
+{
+    [TestFixture]
+    public class CreateOrderTests
+    {
+        private Mock<IOrderService> _serviceMock;
+        [SetUp]
+        public void Setup()
+        {
+            _serviceMock = new Mock<IOrderService>();
+        }
+        [Test]
+        public async Task CreateOrder_Successfully()
+        {
+            _serviceMock.Setup(s => s.CreateOrder(It.IsAny<CreateOrder>())).ReturnsAsync(Guid.NewGuid());
+            var result = await _serviceMock.Object.CreateOrder(new CreateOrder());
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOf<Guid>(result);
+        }
+    }
+}
---
+++ Ninja.Infrastructure.UnitTests/ServicesTests/OrderServiceTests/GetAllOrdersTests.cs
@@ -0,0 +1,32 @@
+﻿using Moq;
+using Ninja.Application.Common.Interfaces;
+using Ninja.Domain.Entities.OrderModel;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ninja.Infrastructure.UnitTests.ServicesTests.OrderServiceTests
+{
+    [TestFixture]
+    public class GetAllOrdersTests
+    {
+        private Mock<IOrderService> _serviceMock;
+        [SetUp]
+        public void Setup()
+        {
+            _serviceMock = new Mock<IOrderService>();
+        }
+        [Test]
+        public async Task GetAllOrders_Successfully()
+        {
+            _serviceMock.Setup(s => s.GetOrders()).ReturnsAsync(new List<Order>().AsEnumerable());
+            var result = await _serviceMock.Object.GetOrders();
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOf<IEnumerable<Order>>(result);
+        }
+    }
+}
